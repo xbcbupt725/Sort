ecfc432a0cc50049faad8889915c27c6200f526f
xbcbupt725 <xbcbupt725@gmail.com>
2016-09-12 08:46:05 +0800

    更新了快排算法

--- src/quick_sort.java
+++ src/quick_sort.java
@@ -1,34 +1,51 @@
 public class quick_sort {
-	int[] arr;
-
-	private void swap(int x, int y) {
-		int temp = arr[x];
-		arr[x] = arr[y];
-		arr[y] = temp;
-	}
-
-	private void quick_sort_recursive(int start, int end) {
-		if (start >= end)
-			return;
-		int mid = arr[end];
-		int left = start, right = end - 1;
-		while (left < right) {
-			while (arr[left] < mid && left < right)
-				left++;
-			while (arr[right] >= mid && left < right)
-				right--;
-			swap(left, right);
+	public static void main(String[] args){
+		int a[] = {2,1,4,5,6,3,7,9,8};	
+		sort(a,0,a.length-1);
+		for(int c : a){
+			System.out.println(c);
 		}
-		if (arr[left] >= arr[end])
-			swap(left, end);
-		else
-			left++;
-		quick_sort_recursive(start, left - 1);
-		quick_sort_recursive(left + 1, end);
+		
 	}
-
-	public void sort(int[] arrin) {
-		arr = arrin;
-		quick_sort_recursive(0, arr.length - 1);
+	
+	
+	
+	private static void sort(int[] a, int left, int right)
+	{
+	    if(left >= right)/*如果左边索引大于或者等于右边的索引就代表已经整理完成一个组了*/
+	    {
+	        return ;
+	    }
+	    int i = left;
+	    int j = right;
+	    int key = a[left];
+	     
+	    while(i < j)                               /*控制在当组内寻找一遍*/
+	    {
+	        while(i < j && key <= a[j])
+	        /*而寻找结束的条件就是，1，找到一个小于或者大于key的数（大于或小于取决于你想升
+	        序还是降序）2，没有符合条件1的，并且i与j的大小没有反转*/ 
+	        {
+	            j--;/*向前寻找*/
+	        }
+	         
+	        a[i] = a[j];
+	        /*找到一个这样的数后就把它赋给前面的被拿走的i的值（如果第一次循环且key是
+	        a[left]，那么就是给key）*/
+	         
+	        while(i < j && key >= a[i])
+	        /*这是i在当组内向前寻找，同上，不过注意与key的大小关系停止循环和上面相反，
+	        因为排序思想是把数往两边扔，所以左右两边的数大小与key的关系相反*/
+	        {
+	            i++;
+	        }
+	         
+	        a[j] = a[i];
+	    }
+	     
+	    a[i] = key;/*当在当组内找完一遍以后就把中间数key回归*/
+	    sort(a, left, i - 1);/*最后用同样的方式对分出来的左边的小组进行同上的做法*/
+	    sort(a, i + 1, right);/*用同样的方式对分出来的右边的小组进行同上的做法*/
+	                       /*当然最后可能会出现很多分左右，直到每一组的i = j 为止*/
 	}
 }
