ab9ca151b49bb21fa9c5abdbcd3c9d9a095c713f
xbcbupt725 <xbcbupt725@gmail.com>
2016-09-13 15:28:52 +0800

    更新了插入排序

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,7 +3,7 @@ public class InsertionSort {
 	
 	public static void main(String[] args){
 		int a[] = {2,1,5,6,3,8,4,7,9};
-		insertion_sort1(a);
+		insertion_sort2(a);
 		for(int i : a){
 			System.out.println(i);
 		}	
@@ -30,6 +30,18 @@ public class InsertionSort {
 					arr[j + 1] = arr[j];
 					arr[j] = temp;
 				}
+			}
+		}
+		
+		public static void insertion_sort2(int[] arr){
+			if(arr == null || arr.length <2) return ;
+			for(int i = 1; i < arr.length; ++i){
+				int key = arr[i];
+				int j = i-1;
+				for( ;j >= 0 && arr[j] > key;--j){
+					arr[j+1] = arr[j];
+				}
+				arr[j+1] = key;
 			}
 		}
 }
